88cd43239388493bae1f2d7568139acd7a9b92cb
Sam Verschueren <sam.verschueren@gmail.com>
2017-01-26 10:27:55 +0100

    tweaks

--- test.js
+++ test.js
@@ -9,24 +9,19 @@ function fn(input, optional) {
 	}
 }
 
-function fnFunction(input, fn) {
-	m(input).is(fn || (x => x > 10));
-}
-
-function biggerThenTen(x) {
-	return x > 10;
-}
+const fnFunction = (input, fn) => m(input).is(fn || (x => x > 10));
+const biggerThenTen = x => x > 10;
 
 test('throw error if type is incorrect', t => {
 	t.throws(fn.bind(undefined, 'foo'), TypeError);
 });
 
 test('not throws error if type is correct', t => {
-	t.doesNotThrow(fn.bind(undefined, 5));
+	t.notThrows(fn.bind(undefined, 5));
 });
 
 test('not throws error if parameter is optional and not provided', t => {
-	t.doesNotThrow(fn.bind(undefined, undefined, true));
+	t.notThrows(fn.bind(undefined, undefined, true));
 });
 
 test('throw error if parameter is optional and type is incorrect', t => {
@@ -36,5 +31,5 @@ test('throw error if parameter is optional and type is incorrect', t => {
 test('validate function', t => {
 	t.throws(fnFunction.bind(undefined, 5), '5 did not pass the test');
 	t.throws(fnFunction.bind(undefined, 5, biggerThenTen), '5 did not pass the test of function `biggerThenTen`');
-	t.doesNotThrow(fnFunction.bind(undefined, 15));
+	t.notThrows(fnFunction.bind(undefined, 15));
 });
